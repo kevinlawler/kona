5eae9ff7b01154657018cb09479c051470b3303d
Tom Szczesny <tavmem@gmail.com>
2013-05-10 12:03:40 -0400

    put all lib functions in d.c - #186

--- d.h
+++ d.h
@@ -18,9 +18,3 @@ typedef struct k0{I c,t,n;struct k0*k[1];}*K;
 extern K Ki(I);
 extern K Kf(F);
 extern K newK(I t, I n);
-
-K f(K x,K y){R Ki(kI(x)[0]+ kI(y)[0]);}
-K g(K x){R Kf(kF(x)[0]+1);} 
-K h(K a,K b,K c,K d,K e,K f,K g,K h){R Ki(123);} 
-
-
--- myadd.c
+++
@@ -1,3 +0,0 @@
-#include "d.h"
-
-K myadd (K x, K y){I a=*kI(x); I b=*kI(y); R Ki(a+b);}
