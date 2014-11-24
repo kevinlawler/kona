1ab9e3c2a2daf2b7a6adb9a58bfad099843261a0
Tom Szczesny <tavmem@gmail.com>
2014-11-24 16:42:17 -0500

    fnc may not always exist & needs to be re-initialized

--- src/kc.c
+++ src/kc.c
@@ -191,8 +191,10 @@ cleanup:
     if(fError) {
       if(Line) {
         O("%s\n",Line); I cnt=0,i;
-        for(i=0;i<strlen(Line);i++) { if(Line[i]==*fnc) cnt=cnt+1; }
-        if(cnt==1) { S ptr=strchr(Line,*fnc); DO(ptr-Line,O(" ")) O("^\n"); }
+        if(fnc) {
+          for(i=0;i<strlen(Line);i++) { if(Line[i]==*fnc) cnt=cnt+1; }
+          if(cnt==1) { S ptr=strchr(Line,*fnc); DO(ptr-Line,O(" ")) O("^\n"); }
+        }
         check();          //enter suspended execution mode for checking 
       }
       else O("%s\n",*a);
@@ -209,7 +211,7 @@ done:
     fWksp=0;
   }
   if(o && !fLoad)prompt(b+fCheck); 
-  kerr("undescribed"); fer=0; 
+  kerr("undescribed"); fer=0; fnc=0;
   R c;
 }
 
