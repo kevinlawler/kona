7ed2c6731b710005cff5866e8da17a56d87309a9
Tom Szczesny <tavmem@gmail.com>
2015-10-16 23:23:40 -0400

    fix remaining case from pull request #379

--- src/p.c
+++ src/p.c
@@ -115,6 +115,7 @@ Z I mark_name(S s,I n,I i,I*m)
     while(i+c<n&&isalnum_(s[i+c]))c++;if(i+c>=n)break;
     if('.'==s[i+c])c++;               
   }
+  if(c!=0 && '.'==s[i-1] && m[i-2]==0)c=0;
   R c;
 }
 
