8c675d2f0f30686fe1291632837e0249220129cb
Tom Szczesny <tavmem@gmail.com>
2015-12-25 21:53:46 -0500

    partial fix for #391: 'm:{:[x;1;2]};{m 3}5;{m 3}5' case

--- src/tests.c
+++ src/tests.c
@@ -249,6 +249,7 @@ Z I tests02()
   TC(2, mi:{:[x!2;(x-1)%2; _ ((x-1),x)%2.0]}; {mi x}5; {mi x}5)                //issue #391
   TC(1, m:{:[x!2; 1; 2]}; {m x}5; {m x}5)                                      //issue #391
   TC(1, m:{:[x; 1; 2]}; {m x}5; {m x}5)                                        //issue #391
+  TC(1, m:{:[x; 1; 2]}; {m 3}5; {m 3}5)                                        //issue #391
 
   //Error trap: {[a;b][c;d] a+b} -> parse error ; { {[a][b] }} -> parse error
   TC(.[*; (3;4); :], (0;12) )
