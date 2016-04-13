607d821224f6a79847634fd2af8b1e3237176f44
Tom Szczesny <tavmem@gmail.com>
2016-04-13 16:50:02 -0400

    fix #425: \d does not handle root directories

--- src/c.c
+++ src/c.c
@@ -453,29 +453,17 @@ Z K backslash_d(S s,I n,K*dict) {
   C z[256];
   //I len=strlen(d_); if(n==2){K r=newK(-3,len); strncpy(kC(r),d_,len); R r;}  // yields contents of d_ enclosed in quotes
   if(n==2) {O("%s\n",d_); R _n();}  // yields contents of d_ without quotes (same as k3.2)
-  if(n==4 && s[3]==*".") { d_=(S)sp(""); R _n();}
-  if(n==4 && s[3]==*"^") {
+  if(n==4 && s[3]=='.') { d_=(S)sp(""); R _n();}
+  if(n==4 && s[3]=='^') {
     if(strlen(d_)==0) R _n();
     if(strlen(d_)==2) {d_=(S)sp(""); R _n();}
-    if(strlen(d_)>3) {
-      I c=0,i=0;
-      for(i=0;i<strlen(d_);i++) if(d_[i]==*".")c=i;
-      strcpy(z,d_); z[c]=*"\0"; d_=(S)sp(z);
-      R _n();
-    }
-  }
-  // \d .k
-  if(n==5 && s[3]==*"." && s[4]==*"k") { d_=(S)sp(".k"); R _n();}
-  if(n==5 && s[3]==*"." && s[4]!=*"k") {O("absolute backslash-d should begin with .k\n"); R _n();} 
-  if(isalpha(s[3])) { 
-    denameD(dict,s+3,1);
-    strcpy(z,d_); strcat(z,"."); strcat(z,s+3); d_=(S)sp(z);
-    R _n();
-  }
-  if(n>=6 && s[3]==*"." && s[4]==*"k" && s[5]==*".") {denameD(&KTREE,s+3,1); d_=(S)sp(s+3); R _n();}
-  if(n>=6 && s[3]==*"." && (s[4]!=*"k" || s[5]!=*".")) {O("absolute backslash-d should begin with .k.\n"); R _n();}
-  R NYI;
-}
+    if(strlen(d_)>3){ I c=0,i=0; for(i=0;i<strlen(d_);i++)if(d_[i]=='.')c=i; strcpy(z,d_); z[c]='\0'; d_=(S)sp(z); R _n(); } }
+  if(n==5 && s[3]=='.' && s[4]=='k') { d_=(S)sp(".k"); R _n();}
+  if(n==5 && s[3]=='.' && s[4]!='k') {O("absolute backslash-d should begin with .k\n"); R _n();} 
+  if(isalpha(s[3])){ denameD(dict,s+3,1); strcpy(z,d_); strcat(z,"."); strcat(z,s+3); d_=(S)sp(z); R _n(); }
+  if(n>=6 && s[3]=='.' && s[4]=='k' && s[5]=='.'){denameD(&KTREE,s+3,1); d_=(S)sp(s+3); R _n();}
+  if(s[3]=='.'){denameD(&KTREE,s+3,1); d_=(S)sp(s+3); R _n();}
+  R NYI; }
 
 Z K backslash_v(S s,I n,K*dict) {
   C z[256]; z[0]='\0';
