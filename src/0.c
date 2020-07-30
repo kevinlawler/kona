c556aceb0776f910ddd1ef3b9b07653fd8a863bf
Tom Szczesny <tavmem@gmail.com>
2020-07-29 22:18:11 -0400

    add strsep version for Windows

--- src/0.c
+++ src/0.c
@@ -57,6 +57,53 @@ Z K readVector(K x,I t);
 Z I sendall(I s,S b,I k);
 Z K _5d_(K x,K y,I dosync);
 
+#if defined(WIN32)
+/*  (modified version of copyrighted strsep for WIN32)
+ * Copyright (c) 1990, 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the University of
+ *	California, Berkeley and its contributors.
+ * 4. Neither the name of the University nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+*/
+S strsep(S *stringp, cS delim)
+{ S s; cS spanp; int c, sc; S tok;
+  if((s = *stringp) == NULL) R (NULL);
+  for (tok = s;;) {
+    c = *s++;
+    spanp = delim;
+    do { if ((sc = *spanp++) == c) {
+      if(c == 0) s = NULL;
+      else s[-1] = 0;
+      *stringp = s;
+      R (tok); }
+    } while (sc != 0); } }
+#endif
+
 Z V freopen_stdin() {
 #if defined(__OpenBSD__)
   return freopen("/dev/stdin","r",stdin);
@@ -972,7 +1019,7 @@ Z void execute(S *argvP, I fWait) {
   si.cb = sizeof(si);
   ZeroMemory( &pi, sizeof(pi) );
   if( !CreateProcess(NULL,argv,NULL,NULL,FALSE,0,NULL,NULL,&si,&pi) ) {
-    O( "CreateProcess failed (%d).\n", GetLastError() ); R; }
+    O( "CreateProcess failed (%ld).\n", GetLastError() ); R; }
   if(fWait) WaitForSingleObject( pi.hProcess, INFINITE );
   CloseHandle( pi.hProcess );
   CloseHandle( pi.hThread ); }
