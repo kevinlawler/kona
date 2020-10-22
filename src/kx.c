bc09913f8dc222ac942e1fe76a6ffdba324f8542
Tom Szczesny <tavmem@gmail.com>
2020-10-22 03:55:10 -0400

    fix #605: Case 83 for issue 468

--- src/kx.c
+++ src/kx.c
@@ -862,6 +862,7 @@ Z K ex2(V*v, K k)  //execute words --- all returns must be Ks. v: word list, k: 
     if(1!=sva(v[1])) d=ex1(v+(offsetColon==v[1]?2:3),k,0,0,1);   // oom -- except it's ok for d to be 0 elsewhere
     d=bk(d)?0:d;
     if(fer>0) { cd(c); cd(d); cd(b); R _n(); }
+    if(!*w)R PE;
     if(cirRef(*w,d) || (((*w)->t==6 && d) && (d->t==0 || d->t==5 || ABS(d->t)!=d->t)))
     { K x = d; if(rc(x)){ d=kclone(x); cd(x); } }
     else if((*w)->t!=6){ K x = *w; if(rc(x)>1){ *w=kclone(x); cd(x); } }
