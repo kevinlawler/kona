ef6c42da8e21cd2c5b4e405b966d0852cb66d6f5
Tom Szczesny <tavmem@gmail.com>
2022-01-08 16:57:54 -0500

    fix #614

--- src/kx.c
+++ src/kx.c
@@ -191,7 +191,7 @@ Z K overMonad(K a, V *p, K b)
              if(flag) break;
              u=c?c:u; U(c=dv_ex(0,p-1,u))
              if(1==ABS(b->t) && 3==ABS(c->t)) flag=1; }
-         else if(*o<(V)DT_SIZE || 7==(*(K*)*o)->t)  //f is a function
+         else if(*o<(V)DT_SIZE || ((7==(*(K*)*o)->t) && 3==(*(K*)*o)->n))  //f is a function
               { while(1)
                 { if(matchI(b,c) || (u!=b && matchI(u,c))) flag=1;
                   if(flag)break;
